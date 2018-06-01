92fec4d21fcb5022dcac14aefd97a276efbb97de
Pierce Primm, Sr <pierce@primm.cc>
2018-06-01 11:45:18 -0500

    Remove credentials from app module

--- src/app/index.js
+++ src/app/index.js
@@ -35,34 +35,3 @@ export default Module(({ controller }) => {
     }
   }
 })
-
-
-/** MQTTModule for LOCAL EMQ BROKER
-      mqtt: MQTTModule({
-        mqttUrl: 'ws://10.10.101.29:8083/mqtt',
-        mqttOptions: {
-          username: 'webClient',
-          password: 'public',
-          keepAlive: 10,
-          queueQoSZero: false,
-        },
-        deviceMqttRoot: 'dev',
-        deviceList: ['dev01','dev02','dev03','dev04','dev05'],
-        viewRoot: 'view',
-      }),
-    
-    MQTTModule for CloudMQTT
-    mqtt: MQTTModule({
-        mqttUrl: 'wss://m11.cloudmqtt.com:30876',
-        mqttOptions: {
-          username: 'TestUser',
-          password: 'hn_nIjPg3ffQ',
-          keepAlive: 10,
-          queueQoSZero: false,
-        },
-        deviceMqttRoot: 'dev',
-        deviceList: ['dev01','dev02','dev03','dev04','dev05'],
-        viewRoot: 'view',
-      }),
-
-*/
